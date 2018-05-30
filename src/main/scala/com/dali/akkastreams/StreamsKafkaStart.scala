e8e9413ad5c8072c35845e10c82c825f244967d3
Mohamed Ali Bannour <>
2018-05-30 18:45:42 +0200

    fixed the some issues.

--- src/main/scala/com/dali/akkastreams/StreamsKafkaStart.scala
+++ src/main/scala/com/dali/akkastreams/StreamsKafkaStart.scala
@@ -1,13 +1,19 @@
 package com.dali.akkastreams
 
+import java.lang.Long
 import java.util.Properties
+import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
-import java.{lang, util}
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.serialization._
+import org.apache.kafka.common.utils.Bytes
 import org.apache.kafka.streams._
-import org.apache.kafka.streams.kstream.{KStream, Produced}
+import org.apache.kafka.streams.kstream.{KStream, Materialized, Produced}
+import org.apache.kafka.streams.state.KeyValueStore
+
+import scala.collection.JavaConverters.asJavaIterableConverter
+
 
 object StreamsKafkaStart extends App {
 
@@ -27,16 +33,20 @@ object StreamsKafkaStart extends App {
 
   val textLines: KStream[String, String] = builder.stream("streams-plaintext-input", Consumed.`with`(stringSerde, stringSerde))
 
-  val worldCounts: KStream[String, lang.Long] = textLines.flatMapValues(value => util.Arrays.asList(pattern.split(value.toLowerCase())))
-     .groupByKey()
-     .count()
-     .toStream()
+  val worldCounts: KStream[String, Long] = textLines
+    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+").toIterable.asJava)
+    .groupBy((_, word) => word)
+    .count(Materialized.as("counts-store").asInstanceOf[Materialized[String, Long, KeyValueStore[Bytes, Array[Byte]]]])
+    .toStream()
 
+    worldCounts.to("streams-wordcount-output", Produced.`with`(Serdes.String(), Serdes.Long()))
 
-  worldCounts.to("streams-wordcount-output", Produced.`with`(stringSerde, longSerde))
-
-
-  val streams =  new KafkaStreams(builder.build(), config);
+  val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
   streams.start()
+
+  Runtime.getRuntime.addShutdownHook(new Thread(() => {
+    streams.close(10, TimeUnit.SECONDS)
+  }))
+
 
 }
